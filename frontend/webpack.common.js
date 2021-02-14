1ff9d5fde1a2000c792b3a0a8225dd66b8b9d615
yantene <contact@yantene.net>
2021-02-14 20:54:59 +0900

    キャッシュ対策で生成ファイル名に content hash を付与

--- frontend/webpack.common.js
+++ frontend/webpack.common.js
@@ -12,7 +12,7 @@ module.exports = {
   },
   output: {
     path: path.resolve(__dirname, "dist"),
-    filename: "[name].js",
+    filename: "[name].[contenthash].js",
   },
   module: {
     rules: [
