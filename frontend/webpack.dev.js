3965010ce809ebaed27358c632bddd2060a82180
yantene <contact@yantene.net>
2021-02-14 20:11:41 +0900

    sourcemap 導入

--- frontend/webpack.dev.js
+++ frontend/webpack.dev.js
@@ -4,6 +4,7 @@ const commonConfig = require("./webpack.common.js");
 
 module.exports = merge(commonConfig, {
   mode: "development",
+  devtool: "eval-cheap-module-source-map",
   devServer: {
     open: true,
     port: 8080,
