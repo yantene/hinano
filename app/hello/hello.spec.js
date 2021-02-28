824c69d5a0b8b34f9a88f57fe5fc644286560d96
yantene <contact@yantene.net>
2021-02-28 16:45:05 +0900

    不要な js ファイルが含まれていたので削除

--- app/hello/hello.spec.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var hello_1 = require("./hello");
-describe("hello", function () {
-    test("should say hello to yantene.", function () {
-        var hello = new hello_1.Hello("yantene");
-        expect(hello.greet()).toBe("Hello, yantene!");
-    });
-});
